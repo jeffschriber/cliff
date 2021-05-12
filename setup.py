841094ee9ce09458dcd3050dc3b0448c9e069d77
jeffschriber <jbschri@emory.edu>
2021-05-12 15:02:54 -0400

    update setup.py to add tar files

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
     # Customize MANIFEST.in if the general case does not suit your needs
     # Comment out this line to prevent the files from being packaged with your software
     include_package_data=True,
+    package_data={'cliff':['cliff/models/large/adens/*.tar', 'cliff/models/large/hirsh/*.tar','cliff/models/large/mtp/*.tar']},
 
     # Allows `setup.py test` to work correctly with pytest
     setup_requires=[] + pytest_runner,
