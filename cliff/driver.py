626f7567f4a8f7bfacd45bdb7ebea782cdac1255
jeffschriber <jbschri@emory.edu>
2021-05-18 12:48:54 -0400

    t32

--- cliff/driver.py
+++ cliff/driver.py
@@ -184,10 +184,11 @@ def predict_from_dimers(dimers, load_path=None, return_pairs=False, infile=None,
             mon_a = load_atomic_properties(mon_a,load_path)  
             mon_b = load_atomic_properties(mon_b,load_path)  
 
-        try:
-            en = energy_kernel(mon_a, mon_b, options) 
-        except:
-            en = "Error"
+        en = energy_kernel(mon_a, mon_b, options) 
+        #try:
+        #    en = energy_kernel(mon_a, mon_b, options) 
+        #except:
+        #    en = "Error"
 
         energies.append(en)
 
